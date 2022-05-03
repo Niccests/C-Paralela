223503cfa4b284c3fea371e7ae77399d8503cc73
Niccests <101159770+Niccests@users.noreply.github.com>
2022-05-03 05:37:20 -0500

    Add files via upload

---
+++ script.sh
@@ -0,0 +1,163 @@
+#!/bin/bash
+for i in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36
+do
+	./MM1cOMP 100 2
+	
+	
+done
+siguiente="200"
+echo $siguiente
+
+for i in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36
+do
+	./MM1cOMP 200 2
+	
+	
+done
+
+siguiente="300"
+echo $siguiente
+
+for i in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36
+do
+	./MM1cOMP 300 2
+	
+	
+done
+siguiente="400"
+echo $siguiente
+
+for i in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36
+do
+	./MM1cOMP 400 2
+	
+	
+done
+
+siguiente="500"
+echo $siguiente
+
+for i in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36
+do
+	./MM1cOMP 500 2
+	
+	
+done
+
+siguiente="600"
+echo $siguiente
+
+for i in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36
+do
+	./MM1cOMP 600 2
+	
+	
+done
+siguiente="700"
+echo $siguiente
+
+for i in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36
+do
+	./MM1cOMP 700 2
+	
+	
+done
+siguiente="800"
+echo $siguiente
+
+for i in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36
+do
+	./MM1cOMP 800 2
+	
+	
+done
+
+siguiente="900"
+echo $siguiente
+
+for i in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36
+do
+	./MM1cOMP 900 2
+	
+	
+done
+siguiente="1000"
+echo $siguiente
+
+for i in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36
+do
+	./MM1cOMP 1000 2
+	
+	
+done
+
+siguiente="1100"
+echo $siguiente
+
+for i in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36
+do
+	./MM1cOMP 1100 2
+	
+	
+done
+
+siguiente="1200"
+echo $siguiente
+
+for i in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36
+do
+	./MM1cOMP 1200 2
+	
+	
+done
+
+siguiente="1300"
+echo $siguiente
+
+for i in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36
+do
+	./MM1cOMP 1300 2
+	
+	
+done
+
+siguiente="1400"
+echo $siguiente
+
+for i in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36
+do
+	./MM1cOMP 1400 2
+	
+	
+done
+
+siguiente="1500"
+echo $siguiente
+
+for i in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36
+do
+	./MM1cOMP 1500 2
+	
+	
+done
+
+siguiente="1600"
+echo $siguiente
+
+for i in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36
+do
+	./MM1cOMP 1600 2
+	
+	
+done
+
+
+siguiente="1700"
+echo $siguiente
+
+for i in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36
+do
+	./MM1cOMP 1700 2
+	
+	
+done
---
+++ scriptf.sh
@@ -0,0 +1,163 @@
+#!/bin/bash
+for i in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36
+do
+	./MM1fOMP 100 2
+	
+	
+done
+siguiente="200"
+echo $siguiente
+
+for i in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36
+do
+	./MM1fOMP 200 2
+	
+	
+done
+
+siguiente="300"
+echo $siguiente
+
+for i in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36
+do
+	./MM1fOMP 300 2
+	
+	
+done
+siguiente="400"
+echo $siguiente
+
+for i in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36
+do
+	./MM1fOMP 400 2
+	
+	
+done
+
+siguiente="500"
+echo $siguiente
+
+for i in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36
+do
+	./MM1fOMP 500 2
+	
+	
+done
+
+siguiente="600"
+echo $siguiente
+
+for i in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36
+do
+	./MM1fOMP 600 2
+	
+	
+done
+siguiente="700"
+echo $siguiente
+
+for i in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36
+do
+	./MM1fOMP 700 2
+	
+	
+done
+siguiente="800"
+echo $siguiente
+
+for i in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36
+do
+	./MM1fOMP 800 2
+	
+	
+done
+
+siguiente="900"
+echo $siguiente
+
+for i in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36
+do
+	./MM1fOMP 900 2
+	
+	
+done
+siguiente="1000"
+echo $siguiente
+
+for i in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36
+do
+	./MM1fOMP 1000 2
+	
+	
+done
+
+siguiente="1100"
+echo $siguiente
+
+for i in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36
+do
+	./MM1fOMP 1100 2
+	
+	
+done
+
+siguiente="1200"
+echo $siguiente
+
+for i in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36
+do
+	./MM1fOMP 1200 2
+	
+	
+done
+
+siguiente="1300"
+echo $siguiente
+
+for i in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36
+do
+	./MM1fOMP 1300 2
+	
+	
+done
+
+siguiente="1400"
+echo $siguiente
+
+for i in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36
+do
+	./MM1fOMP 1400 2
+	
+	
+done
+
+siguiente="1500"
+echo $siguiente
+
+for i in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36
+do
+	./MM1fOMP 1500 2
+	
+	
+done
+
+siguiente="1600"
+echo $siguiente
+
+for i in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36
+do
+	./MM1fOMP 1600 2
+	
+	
+done
+
+
+siguiente="1700"
+echo $siguiente
+
+for i in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36
+do
+	./MM1fOMP 1700 2
+	
+	
+done
